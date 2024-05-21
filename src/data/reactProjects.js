4973120d06721611e96820720ec92c134f4638af
MikhailKup <mifffa.mk@gmail.com>
2024-05-21 16:20:35 +0500

    Commit 6 Add Calculator

--- src/data/reactProjects.js
+++ src/data/reactProjects.js
@@ -1,15 +1,10 @@
 import marvelImg from '../assets/img/projects/react/marvel.png'
+import calcImg from '../assets/img/projects/react/calculator.png'
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 
  export const reactProjects = [
-	{
-		title: "Marvel Heroes",
-		description: "A React project for obtaining Marvel characters from the official API",
-		imgUrl: marvelImg,
-		link: 'https://mikhailkup.github.io/ReactProject-Marvel/'
-	},
 	{
 		title: "Business Startup",
 		description: "Design & Development",
@@ -26,13 +21,15 @@ import projImg3 from "../assets/img/project-img3.png";
 		imgUrl: projImg1,
 	},
 	{
-		title: "Business Startup",
-		description: "Design & Development",
-		imgUrl: projImg2,
+		title: "Marvel Heroes",
+		description: "A React project for obtaining Marvel characters from the official API",
+		imgUrl: marvelImg,
+		link: 'https://mikhailkup.github.io/ReactProject-Marvel/'
 	},
 	{
-		title: "Business Startup",
-		description: "Design & Development",
-		imgUrl: projImg3,
+		title: "Calculator",
+		description: "Standard calculator on React",
+		imgUrl: calcImg,
+		link: 'https://mikhailkup.github.io/ReactProject-Calculator/'
 	},
 ];
--- src/data/rusReactProjects.js
+++ src/data/rusReactProjects.js
@@ -1,15 +1,10 @@
 import marvelImg from '../assets/img/projects/react/marvel.png'
+import calcImg from '../assets/img/projects/react/calculator.png'
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 
  export const rusReactProjects = [
-	{
-		title: "Герои Marvel",
-		description: "Проект React для получения персонажей Marvel из официального API",
-		imgUrl: marvelImg,
-		link: 'https://mikhailkup.github.io/ReactProject-Marvel/'
-	},
 	{
 		title: "Бизнес стартап",
 		description: "Дизайн и разработка",
@@ -26,13 +21,15 @@ import projImg3 from "../assets/img/project-img3.png";
 		imgUrl: projImg1,
 	},
 	{
-		title: "Бизнес стартап",
-		description: "Дизайн и разработка",
-		imgUrl: projImg2,
+		title: "Герои Marvel",
+		description: "Проект React для получения персонажей Marvel из официального API",
+		imgUrl: marvelImg,
+		link: 'https://mikhailkup.github.io/ReactProject-Marvel/'
 	},
 	{
-		title: "Бизнес стартап",
-		description: "Дизайн и разработка",
-		imgUrl: projImg3,
+		title: "Калькулятор",
+		description: "Стандартный калькулятор на React",
+		imgUrl: calcImg,
+		link: 'https://mikhailkup.github.io/ReactProject-Calculator/'
 	},
 ];
